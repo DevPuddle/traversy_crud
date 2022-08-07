52c603f514426765291920950fd5fb35c25d49ab
Isaiah Henson <Devpuddles@gmail.com>
2022-08-07 10:31:57 -0400

    added morgan middleware and a conditional

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const morgan = require('morgan')
 const connectDB = require('./config/db')
 //Load config
 dotenv.config({ path: './config/config.env' })
-
+ 
 connectDB()
 
 const app = express()
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
 
 const PORT = process.env.PORT || 8500
 
