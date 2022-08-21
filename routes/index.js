e286276d9e40e37d2f0696633c597259c5739ef6
Isaiah Henson <Devpuddles@gmail.com>
2022-08-21 18:16:34 -0400

    refactoring code

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,16 @@ const express = require('express')
 const router = express.Router()
 const {ensureAuth, ensureGuest} = require('../middleware/auth')
 
+const Story = require('../models/Story')
+
 //@desc Login/landing page
 //@route GET /
-router.get('/', ensureGuest, (req,res) => {
+router.get('/', ensureGuest, async (req,res) => {
+    try {
+        const stories = await Story.find({user: req.user.id})
+    } catch (error) {
+        
+    }
     res.render('login', {
         layout: 'login',
     })
@@ -12,9 +19,8 @@ router.get('/', ensureGuest, (req,res) => {
 
 //@desc Dashboard
 //@route GET /dashboard
-router.get('/dashboard', ensureAuth, (req,res) => {
-    console.log(req.user)
-    res.render('dashboard',{
+router.get('/dashboard', ensureAuth, (req, res) => {
+    res.render('dashboard', {
         name: req.user.firstname,
     })
 })
