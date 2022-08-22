bf3285b48937d50ae8575fdfa18a246e05efbed2
Isaiah Henson <Devpuddles@gmail.com>
2022-08-22 17:40:39 -0400

    added try catch methods to stories.js

--- routes/stories.js
+++ routes/stories.js
@@ -44,41 +44,64 @@ router.get('/', ensureAuth, async (req, res) => {
 //@desc Show edit page
 //@route GET /stories/edit/:id
 router.get('/edit/:id', ensureAuth, async (req, res) => {
-    const story = await Story.find({
-        _id: req.params.id
-    }).lean()
+    try {
+        const story = await Story.find({
+            _id: req.params.id
+        }).lean()
 
-    if (!story) {
-        return res.render('error/404')
-    }
+        if (!story) {
+            return res.render('error/404')
+        }
 
-    if (story.user != req.user.id) {
-        res.redirect('/stories')
-    } else {
-        res.render('stories/edit', {
-            story,
-        })
+        if (story.user != req.user.id) {
+            res.redirect('/stories')
+        } else {
+            res.render('stories/edit', {
+                story,
+            })
+        }
+    } catch (err) {
+        console.error(err)
+        return res.render('error/500')
     }
 })
 //@desc Udate story
 //@route PUT /stories/:id
-router.PUT('/:id', ensureAuth, async (req, res) => {
-    let story = await Story.findById(req.params.id).lean()
+router.put('/:id', ensureAuth, async (req, res) => {
+    try {
+        let story = await Story.findById(req.params.id).lean()
 
-    if (!story) {
-        return res.render('error / 404')
+        if (!story) {
+            return res.render('error / 404')
+        }
+
+        if (story.user != req.user.id) {
+            res.redirect('/stories')
+        } else {
+            story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
+                new: true,
+                runValidators: true
+            })
+            res.redirect('/dashboard')
+        }
     }
-
-    if (story.user != req.user.id) {
-        res.redirect('/stories')
-    } else {
-        story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
-            new: true,
-            runValidators: true
-        })
+    catch (err) {
+        console.error(err)
+        return res.render('error/500')
     }
 })
+//@desc Delete story
+//@route DELETE /stories/:ID
+router.delete('/:id', ensureAuth, async (req, res) => {
+    try {
+        await Story.remove({ _id: req.params.id })
+        res.redirect('/dashboard')
+    } catch (err) {
+        console.error(err)
+        return res.render('error/500')
 
-
+    }
+})
+})
 
 module.exports = router
